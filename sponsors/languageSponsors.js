5688d205598179e67efa81ff0ab09f7eb9319be6
Marc <marc.redolad@estudiantat.upc.edu>
2025-08-03 19:30:37 +0200

    Implemented the new language feature

---
+++ sponsors/languageSponsors.js
@@ -0,0 +1,360 @@
+const aboutEs = {
+  home: "Inicio",
+  events: "Eventos",
+  sponsors: "Sponsors",
+  about_us: "Sobre Nosotros",
+  history: "Historia",
+  competitions: "Competiciones",
+  language_es: "ES",
+  language_en: "ENG",
+  language_cat: "CAT",
+  contact: "Contacto",
+  languages: "IDIOMAS",
+  social: "REDES",
+  follow_us: "SIGUE A PUCRA!",
+  address: "Dirección",
+  contact3: "Contactos",
+  links: "Links",
+  actual_year: "2024", // si es dinámico, puedes omitirlo o actualizarlo por JS
+  sponsors_description: "ELLOS LO HACEN POSIBLE",
+  sponsor_benefits_title: "¿Por qué ser patrocinador de PUCRA?",
+
+  PUCRA_sponsors: [
+    {
+      sponsor_subtitle: "AUTOMATIZACIÓN INDUSTRIAL",
+      sponsor_description:
+        "El progreso tecnológico es el motor del cambio en la vida moderna. Durante más de 90 años, Mitsubishi Electric ha contribuido a este proceso con tecnología de vanguardia, innovación y productos de gran calidad.",
+      web_place: "Sitio Web",
+    },
+    {
+      sponsor_subtitle: "SOLUCIONES DE ALMACENAJE",
+      sponsor_description:
+        "Mecalux es líder en soluciones de almacenaje y sistemas automatizados. Con más de 50 años de experiencia, ofrece innovación, tecnología y eficiencia para la logística y la gestión de mercancías.",
+      web_place: "Sitio Web",
+    },
+    {
+      sponsor_subtitle: "AUTOMATIZACIÓN INDUSTRIAL",
+      sponsor_description:
+        "Líder mundial en automatización y gestión de la energía. Schneider Electric impulsa la transformación digital con soluciones eficientes y sostenibles para la industria moderna.",
+      web_place: "Sitio Web",
+    },
+    {
+      sponsor_subtitle: "MUTUA DE LOS INGENIEROS",
+      sponsor_description:
+        "La Mutua de los Ingenieros ofrece soluciones aseguradoras, financieras y de previsión social adaptadas a las necesidades de ingenieros y profesionales técnicos. Comprometida con la protección y el futuro de sus mutualistas.",
+      web_place: "Sitio Web",
+    },
+    {
+      sponsor_subtitle: "INGENIERÍA INDUSTRIAL DE CATALUÑA",
+      sponsor_description:
+        "Referente institucional que impulsa el desarrollo profesional, la innovación y el compromiso social de la ingeniería industrial en Cataluña. El Colegio promueve el talento y la excelencia técnica.",
+      web_place: "Sitio Web",
+    }
+  ],
+
+  sponsor_benefits: [
+    {
+      sponsor_box_title: "TENDRÁS MÁXIMA VISIBILIDAD",
+      sponsor_box_text:
+        "Tus productos llegarán a ingenieros y profesionales de todo el mundo gracias a nuestra red global."
+    },
+    {
+      sponsor_box_title: "PRESENCIA EN LOS EVENTOS",
+      sponsor_box_text:
+        "Promocionaremos tu marca en nuestros eventos y competiciones junto a la UPC."
+    },
+    {
+      sponsor_box_title: "PUBLICIDAD EN REDES SOCIALES",
+      sponsor_box_text:
+        "Difusión de tu empresa en nuestras redes como Instagram y LinkedIn."
+    },
+    {
+      sponsor_box_title: "LOGO EN LA CAMISETA",
+      sponsor_box_text:
+        "Incluirémos tu logo en la camiseta de competición que llevamos a eventos internacionales."
+    },
+    {
+      sponsor_box_title: "TALENTO JOVEN Y ALTAMENTE CUALIFICADO",
+      sponsor_box_text:
+        "Acceso directo a estudiantes de la UPC para prácticas y futuros proyectos profesionales."
+    }
+  ]
+};
+
+
+const aboutEn = {
+  home: "Home",
+  events: "Events",
+  sponsors: "Sponsors",
+  about_us: "About Us",
+  history: "History",
+  competitions: "Competitions",
+  language_es: "ES",
+  language_en: "ENG",
+  language_cat: "CAT",
+  contact: "Contact",
+  languages: "LANGUAGES",
+  social: "SOCIAL",
+  follow_us: "FOLLOW PUCRA!",
+  address: "Address",
+  contact3: "Contacts",
+  links: "Links",
+  actual_year: "2024",
+  sponsors_description: "THEY MAKE IT POSSIBLE",
+  sponsor_benefits_title: "Why become a PUCRA sponsor?",
+
+  PUCRA_sponsors: [
+    {
+      sponsor_subtitle: "INDUSTRIAL AUTOMATION",
+      sponsor_description:
+        "Technological progress is the driving force of change in modern life. For over 90 years, Mitsubishi Electric has contributed to this process with cutting-edge technology, innovation, and high-quality products.",
+      web_place: "Website",
+    },
+    {
+      sponsor_subtitle: "STORAGE SOLUTIONS",
+      sponsor_description:
+        "Mecalux is a leader in storage solutions and automated systems. With over 50 years of experience, it provides innovation, technology, and efficiency for logistics and goods management.",
+      web_place: "Website",
+    },
+    {
+      sponsor_subtitle: "INDUSTRIAL AUTOMATION",
+      sponsor_description:
+        "A global leader in automation and energy management. Schneider Electric drives digital transformation with efficient and sustainable solutions for modern industry.",
+      web_place: "Website",
+    },
+    {
+      sponsor_subtitle: "ENGINEERS' MUTUAL",
+      sponsor_description:
+        "The Engineers' Mutual offers insurance, financial, and social security solutions tailored to the needs of engineers and technical professionals. Committed to the protection and future of its members.",
+      web_place: "Website",
+    },
+    {
+      sponsor_subtitle: "INDUSTRIAL ENGINEERING OF CATALONIA",
+      sponsor_description:
+        "An institutional benchmark promoting professional development, innovation, and social commitment of industrial engineering in Catalonia. The College supports talent and technical excellence.",
+      web_place: "Website"
+    }
+  ],
+
+  sponsor_benefits: [
+    {
+      sponsor_box_title: "MAXIMUM VISIBILITY",
+      sponsor_box_text:
+        "Your products will reach engineers and professionals worldwide through our global network."
+    },
+    {
+      sponsor_box_title: "PRESENCE AT EVENTS",
+      sponsor_box_text:
+        "We will promote your brand at our events and competitions alongside the UPC."
+    },
+    {
+      sponsor_box_title: "SOCIAL MEDIA ADVERTISING",
+      sponsor_box_text:
+        "Promotion of your company through our networks like Instagram and LinkedIn."
+    },
+    {
+      sponsor_box_title: "LOGO ON THE T-SHIRT",
+      sponsor_box_text:
+        "We will include your logo on the competition shirt we wear at international events."
+    },
+    {
+      sponsor_box_title: "YOUNG AND HIGHLY QUALIFIED TALENT",
+      sponsor_box_text:
+        "Direct access to UPC students for internships and future professional projects."
+    }
+  ]
+};
+
+
+const aboutCat = {
+  home: "Inici",
+  events: "Esdeveniments",
+  sponsors: "Sponsors",
+  about_us: "Sobre Nosaltres",
+  history: "Història",
+  competitions: "Competicions",
+  language_es: "ES",
+  language_en: "ENG",
+  language_cat: "CAT",
+  contact: "Contacte",
+  languages: "IDIOMES",
+  social: "XARXES",
+  follow_us: "SEGUEIX PUCRA!",
+  address: "Adreça",
+  contact3: "Contactes",
+  links: "Enllaços",
+  actual_year: "2024",
+  sponsors_description: "ELLS HO FAN POSSIBLE",
+  sponsor_benefits_title: "Per què ser patrocinador de PUCRA?",
+
+  PUCRA_sponsors: [
+    {
+      sponsor_subtitle: "AUTOMATITZACIÓ INDUSTRIAL",
+      sponsor_description:
+        "El progrés tecnològic és el motor del canvi en la vida moderna. Durant més de 90 anys, Mitsubishi Electric ha contribuït a aquest procés amb tecnologia capdavantera, innovació i productes d’alta qualitat.",
+      web_place: "Lloc web"
+    },
+    {
+      sponsor_subtitle: "SOLUCIONS D’EMMAGATZEMATGE",
+      sponsor_description:
+        "Mecalux és líder en solucions d’emmagatzematge i sistemes automatitzats. Amb més de 50 anys d’experiència, ofereix innovació, tecnologia i eficiència per a la logística i la gestió de mercaderies.",
+      web_place: "Lloc web"
+    },
+    {
+      sponsor_subtitle: "AUTOMATITZACIÓ INDUSTRIAL",
+      sponsor_description:
+        "Líder mundial en automatització i gestió de l’energia. Schneider Electric impulsa la transformació digital amb solucions eficients i sostenibles per a la indústria moderna.",
+      web_place: "Lloc web"
+    },
+    {
+      sponsor_subtitle: "MÚTUA DELS ENGINYERS",
+      sponsor_description:
+        "La Mútua dels Enginyers ofereix solucions asseguradores, financeres i de previsió social adaptades a les necessitats d’enginyers i professionals tècnics. Compromesa amb la protecció i el futur dels seus mutualistes.",
+      web_place: "Lloc web"
+    },
+    {
+      sponsor_subtitle: "ENGINYERIA INDUSTRIAL DE CATALUNYA",
+      sponsor_description:
+        "Referent institucional que impulsa el desenvolupament professional, la innovació i el compromís social de l’enginyeria industrial a Catalunya. El Col·legi promou el talent i l’excel·lència tècnica.",
+      web_place: "Lloc web"
+    }
+  ],
+
+  sponsor_benefits: [
+    {
+      sponsor_box_title: "MÀXIMA VISIBILITAT",
+      sponsor_box_text:
+        "Els teus productes arribaran a enginyers i professionals d’arreu del món gràcies a la nostra xarxa global.",
+      
+    },
+    {
+      sponsor_box_title: "PRESÈNCIA ALS ESDEVENIMENTS",
+      sponsor_box_text:
+        "Promocionarem la teva marca als nostres esdeveniments i competicions conjuntament amb la UPC.",
+      
+    },
+    {
+      sponsor_box_title: "PUBLICITAT A LES XARXES SOCIALS",
+      sponsor_box_text:
+        "Difusió de la teva empresa a les nostres xarxes com Instagram i LinkedIn.",
+      
+    },
+    {
+      sponsor_box_title: "LOGO A LA SAMARRETA",
+      sponsor_box_text:
+        "Inclourem el teu logo a la samarreta de competició que portem a esdeveniments internacionals.",
+      
+    },
+    {
+      sponsor_box_title: "TALENT JOVE I ALTAMENT QUALIFICAT",
+      sponsor_box_text:
+        "Accés directe a estudiants de la UPC per a pràctiques i futurs projectes professionals.",
+      
+    }
+  ]
+};
+
+
+
+function fillSpecificContent(mLang) {
+  document.documentElement.lang = mLang;
+  $("lang-menu").innerText = mLang.toLocaleUpperCase(); // Update the language menu selection
+  // Show or hide language options based on the selected language
+  $("cat").style.display = mLang === "cat" ? "none" : "block";
+  $("en").style.display = mLang === "en" ? "none" : "block";
+  $("es").style.display = mLang === "es" ? " none" : "block";
+
+
+  $("actual-year").innerText = new Date().getFullYear(); // returns the current year
+}
+
+function changeLanguage() {
+  // ../js/language.js
+  let mLang = getLanguage();
+  // Set the language in the document element
+  fillSpecificContent(mLang);
+  let contentLang = null;
+  switch (mLang) {
+    case "cat":
+      contentLang = aboutCat;
+      break;
+    case "en":
+      contentLang = aboutEn;
+      break;
+    default:
+      // Default 'es' if no language is set or if the language is not recognized
+      contentLang = aboutEs;
+      break;
+  }
+  return contentLang;
+}
+
+
+// replace document.getElementById with $ NO ES JQUERY
+const $ = (id) => document.getElementById(id);
+
+// replace document.getElementsByClassName with $$ NO ES JQUERY
+const $$ = (cls) => document.getElementsByClassName(cls);
+
+function fillContentAbout() {
+  console.info(
+    "Hay 10 tipos de personas: Las que saben binario y las que no. Att: Kevin"
+  );
+
+  let contenAbout = changeLanguage();
+
+  // Navbar
+  $("home").innerHTML = contenAbout.home;
+  $("events").innerHTML = contenAbout.events;
+  $("sponsors").innerHTML = contenAbout.sponsors;
+  $("about_us").innerHTML = contenAbout.about_us;
+
+  // Language options
+  $("es").innerHTML = contenAbout.language_es;
+  $("en").innerHTML = contenAbout.language_en;
+  $("cat").innerHTML = contenAbout.language_cat;
+
+
+
+  // Contact
+  $("contact").innerHTML = contenAbout.contact;
+ 
+
+  // Main section title (asegúrate que existe esta clave)
+  if (contenAbout.section_title) {
+    $("title-history").innerHTML = contenAbout.section_title;
+  }
+
+  // Optional fields
+  if ($("sponsors-description"))
+    $("sponsors-description").innerHTML = contenAbout.sponsors_description;
+
+  if ($("sponsor-benefits-title"))
+    $("sponsor-benefits-title").innerHTML = contenAbout.sponsor_benefits_title;
+
+  if ($("actual-year"))
+    $("actual-year").innerHTML = contenAbout.actual_year;
+
+  if ($("web-place"))
+    $("web-place").innerHTML = contenAbout.web_place;
+
+  // Footer
+  $("follow-us").innerHTML = contenAbout.follow_us;
+  $("direction").innerHTML = contenAbout.address;
+  $("links").innerHTML = contenAbout.links;
+
+  // Sponsors
+    for (let i = 0; i < contenAbout.PUCRA_sponsors.length; i++) {
+    const PUCRA_sponsors_item = contenAbout.PUCRA_sponsors[i];
+        $$("sponsor_subtitle")[i].innerHTML = PUCRA_sponsors_item.sponsor_subtitle;
+        $$("sponsor_description")[i].innerHTML = PUCRA_sponsors_item.sponsor_description;
+        $$("web_place")[i].innerHTML = PUCRA_sponsors_item.web_place;
+    }
+
+    for (let i = 0; i < contenAbout.sponsor_benefits.length; i++) {
+    const sponsor_benefits_item = contenAbout.sponsor_benefits[i];
+        $$("sponsor_box_title")[i].innerHTML = sponsor_benefits_item.sponsor_box_title;
+        $$("sponsor_box_text")[i].innerHTML = sponsor_benefits_item.sponsor_box_text;
+    }
+
+}
