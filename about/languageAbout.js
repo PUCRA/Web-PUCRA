3c112ecab7b62c7a4de42fee556e69259ddeb390
zackproject <zackprojectapp@gmail.com>
2025-09-09 14:43:38 +0200

    hide competition & adding animations

--- about/languageAbout.js
+++ about/languageAbout.js
@@ -333,13 +333,17 @@ function changeLanguage() {
   switch (mLang) {
     case "cat":
       contentLang = aboutCat;
+      $("lang-menu").innerText = contentLang.language_cat;
       break;
     case "en":
       contentLang = aboutEn;
+      $("lang-menu").innerText = contentLang.language_en;
       break;
     default:
       // Default 'es' if no language is set or if the language is not recognized
       contentLang = aboutEs;
+      $("lang-menu").innerText = contentLang.language_es;
+
       break;
   }
   return contentLang;
@@ -383,8 +387,6 @@ function fillContentAbout() {
     $$("langs")[i].innerHTML = contenAbout.languages;
   }
 
-  
-  
   // Main Section
   $("title-history").innerHTML = contenAbout.section_title;
 
@@ -393,7 +395,6 @@ function fillContentAbout() {
   $("address").innerHTML = contenAbout.address;
   $("links").innerHTML = contenAbout.links;
   $("contacts").innerHTML = contenAbout.contacts;
-
 
   // Contact
   for (let i = 0; i < $$("contact").length; i++) {
