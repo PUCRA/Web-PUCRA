34aed8e2c8ad4463d95bc341926e82008fad1673
zackproject <zackprojectapp@gmail.com>
2025-09-09 19:19:44 +0200

    adding camera

--- model/main.js
+++ model/main.js
@@ -8,6 +8,9 @@ const camera = new THREE.PerspectiveCamera(
   1000
 );
 
+let zoomxyz = { x: 0, y: 0, z: 0 };
+let zoomPressed = false;
+
 // Create a renderer and add to id="myCanvas"
 const canvas = document.getElementById("myCanvas");
 const renderer = new THREE.WebGLRenderer({ canvas });
@@ -25,5 +28,35 @@ camera.position.z = 5;
 function animate() {
   cube.rotation.x += 0.01;
   cube.rotation.y += 0.01;
+
+  if (zoomPressed) {
+    camera.position.x += zoomxyz.x;
+    camera.position.y += zoomxyz.y;
+    camera.position.z += zoomxyz.z;
+  }
   renderer.render(scene, camera);
+}
+
+const moveObj = document.getElementsByClassName("moveObj");
+//Afegim els event listeners a tots els botons
+for (let i = 0; i < moveObj.length; i++) {
+  moveObj[i].addEventListener("mouseup", () => {
+    zoomPressed = false;
+  });
+  moveObj[i].addEventListener("mousedown", () => {
+    let x = moveObj[i].getAttribute("data-x");
+    let y = moveObj[i].getAttribute("data-y");
+    let z = moveObj[i].getAttribute("data-z");
+    moveCamera(x, y, z);
+  });
+}
+
+function moveCamera(x, y, z) {
+  let speedCamera = 0.03;
+  //Controlem la velocitat de camera
+  zoomxyz.x = x * speedCamera;
+  zoomxyz.y = y * speedCamera;
+  zoomxyz.z = z * speedCamera;
+  //S'executa mentre 'zoomPressed'' es true
+  zoomPressed = true;
 }
