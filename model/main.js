75b80d8bec0360d6cd6933a092d5810e441750a0
zackproject <zackprojectapp@gmail.com>
2025-09-09 18:26:23 +0200

    coment code

--- model/main.js
+++ model/main.js
@@ -2,17 +2,19 @@ import * as THREE from "three";
 
 const scene = new THREE.Scene();
 const camera = new THREE.PerspectiveCamera(
-  75,
+  50,
   window.innerWidth / window.innerHeight,
   0.1,
   1000
 );
 
-const renderer = new THREE.WebGLRenderer();
+// Create a renderer and add to id="myCanvas"
+const canvas = document.getElementById("myCanvas");
+const renderer = new THREE.WebGLRenderer({ canvas });
 renderer.setSize(window.innerWidth, window.innerHeight);
 renderer.setAnimationLoop(animate);
-document.body.appendChild(renderer.domElement);
 
+// Create a cube
 const geometry = new THREE.BoxGeometry(1, 1, 1);
 const material = new THREE.MeshBasicMaterial({ color: 0x00ff00 });
 const cube = new THREE.Mesh(geometry, material);
@@ -23,6 +25,5 @@ camera.position.z = 5;
 function animate() {
   cube.rotation.x += 0.01;
   cube.rotation.y += 0.01;
-
   renderer.render(scene, camera);
 }
